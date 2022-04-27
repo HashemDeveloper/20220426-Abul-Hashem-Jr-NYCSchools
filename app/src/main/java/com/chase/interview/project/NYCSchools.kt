27a443184caf39ae939df346379f8e704eee6580
Abul Hashem Jr <abul@limosys.com>
2022-04-26 21:32:08 -0400

    First commit

---
+++ app/src/main/java/com/chase/interview/project/NYCSchools.kt
@@ -0,0 +1,11 @@
+package com.chase.interview.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NYCSchools : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
