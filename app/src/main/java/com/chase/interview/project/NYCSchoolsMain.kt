db9899c828149b72f4e43db59f42ea2294fa952b
Abul Hashem Jr <abul@limosys.com>
2022-04-27 03:28:13 -0400

    Sets up the view

--- app/src/main/java/com/chase/interview/project/NYCSchoolsMain.kt
+++ app/src/main/java/com/chase/interview/project/NYCSchoolsMain.kt
@@ -2,17 +2,29 @@ package com.chase.interview.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chase.interview.project.base.BaseActivity
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-class NYCSchoolsMain : AppCompatActivity(), HasAndroidInjector {
+class NYCSchoolsMain : BaseActivity(), HasAndroidInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun getLayout(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun getAppTheme(): Int {
+        return R.style.Theme_20220426AbulHashemJrNYCSchools
+    }
+
+    override fun getNavLayout(): Int {
+       return R.navigation.nav_layout
+    }
+
+    override fun getDestination(): Int {
+        return R.id.welcomePage
     }
 
     override fun androidInjector(): AndroidInjector<Any> {
